0c136bf4db48e7b430edead5e7457199c66fa738
rungene <rungenesamuel@gmail.com>
2023-07-27 02:37:09 +0000

    Corrections

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ status: 200, body: 'Success' });
-      console.log('Got a response from the API');
-    } else {
-      reject(Error(''));
-      console.log('Got a response from the API');
-    }
-  });
+  const obj = { status: 200, body: 'success' };
+  return promise
+    .then(() => obj)
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
