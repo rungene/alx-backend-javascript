c57aac80f70ae6f9ec800341f7e55fa6693ae9cd
rungene <rungenesamuel@gmail.com>
2023-07-27 09:10:17 +0000

    Adding comment

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,6 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
+    // we use destructuring to assign the resolved values to the variables
     .then(([resultsPhoto, resultsUser]) => {
       console.log(`${resultsPhoto.body} ${resultsUser.firstName} ${resultsUser.lastName}`);
     })
