eac31f3d234910c966cf2f80ecb4e7213a6a5a3b
rungene <rungenesamuel@gmail.com>
2023-07-28 18:03:14 +0000

    Corrections

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,23 +3,11 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-
-    if (typeof floors !== 'number') {
-      throw TypeError('Floors must be number');
-    }
-
     this._floors = floors;
   }
 
   get floors() {
     return this._floors;
-  }
-
-  set floors(floors) {
-    if (typeof floors !== 'number') {
-      throw TypeError('Floors must be number');
-    }
-    this._floors = floors;
   }
 
   get sqft() {
