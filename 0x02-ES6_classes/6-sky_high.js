e4431b7c503272aac4d40d794a5858d50b5dd25f
rungene <rungenesamuel@gmail.com>
2023-07-28 17:40:49 +0000

    Corections

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -23,6 +23,6 @@ export default class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors.`;
+    return `Evacuate slowly the ${this._floors} floors.`;
   }
 }
