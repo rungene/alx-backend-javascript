a6b7428a13225d2a9dab3da2c41b6c96d8018eba
rungene <rungenesamuel@gmail.com>
2023-07-28 17:36:13 +0000

    Adding main test file and correcting eslint errors

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,28 +1,28 @@
 import Building from './5-building';
 
-export default SkyHighBuilding extends Building {
+export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
 
-    if (typeof floors != 'number') {
-      throw TypeError('Floors must be number');  
+    if (typeof floors !== 'number') {
+      throw TypeError('Floors must be number');
     }
 
-    this._floors = floors
+    this._floors = floors;
   }
 
   get floors() {
-    return this._floors;  
+    return this._floors;
   }
-  
+
   set floors(floors) {
-    if (typeof floors != 'number') {
-      throw TypeError('Floors must be number');  
+    if (typeof floors !== 'number') {
+      throw TypeError('Floors must be number');
     }
-    this._floors = floors
+    this._floors = floors;
   }
 
   evacuationWarningMessage() {
-    return 'Evacuate slowly the NUMBER_OF_FLOORS floors.';  
+    return `Evacuate slowly the ${this.floors} floors.`;
   }
 }
