72c4fca3aa505d1c74634b973ff068d96d4cf843
rungene <rungenesamuel@gmail.com>
2023-07-29 02:44:52 +0000

    Corrections

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,7 +1,3 @@
 export default function uploadPhoto(filename) {
-  return new Promise((reject) => {
-    const error = new Error(`${filename} cannot be processed`);
-
-    reject(error);
-  });
+  return Promise.reject(new Error(`${filename} cannot be processed`));
 }
