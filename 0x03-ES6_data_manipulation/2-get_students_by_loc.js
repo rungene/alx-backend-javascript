16c480775b974e3e7ac07dea87a97f8df34928c8
rungene <rungenesamuel@gmail.com>
2023-08-01 12:18:27 +0000

    Corrections

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
 export default function getStudentsByLocation(students, city) {
-  return students.filter((student) => student.location.toLowerCase().includes(city.toLowerCase()));
+  return students.filter((student) => student.location === city);
 }
