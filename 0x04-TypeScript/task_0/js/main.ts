2f11ea1c07f2efc3302a1402e0b47672066e535c
rungene <rungenesamuel@gmail.com>
2023-08-04 10:52:50 +0000

    Task 0. Creating an interface for a student

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -2,23 +2,52 @@ interface Student {
   firstName: string;
   lastName: string;
   age: number;
+  location: string;
 }
 
-class StudentClass {
+class StudentClass implements Student {
   firstName: string;
   lastName: string;
   age: number;
+  location: string;
 
-  constructor(firstName: string, lastName: string, age: number) {
+  constructor(firstName: string, lastName: string, age: number, location: string) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.age = age;
+    this.location = location;
   }
 }
 
-const student1: Student = new Student();
-const student2: Student = new Student();
+const student1: Student = new StudentClass('Jack', 'Kibe', 20, 'Nairobi');
+const student2: Student = new StudentClass('Clevers', 'Kiprotich', 25, 'Lagos');
 
-const studentsList = [...student1, ...student2];
+const studentsList: Student[] = [student1, student2];
 
+const table: HTMLTableElement = document.createElement('table');
+document.body.appendChild(tb);
 
+let thead: HTMLTableElement = tb.createTHead();
+let trow: HTMLTableElement = thead.insertRow();
+let data = Object.keys(studentsList[0]);
+delete data.lastName;
+delete data.age
+
+for (let key of data) {
+  let th = document.createElement('th');
+  let text: string = document.createTextNode(key);
+  th.appendChild(text);
+  trow.appendChild(th);
+}
+const keySkip = ['lastName', 'age'];
+
+for (let element of studentsList) {
+  let row: HTMLTableElement = table.insertRow();
+  for (key in element) {
+    if (!keySkip.includes(key)) {
+      let cell: HTMLTableElement = row.insertCell();
+      let text = document.createTextNode(element[key]);
+      cell.appendChild(text);
+    }
+  }
+}
