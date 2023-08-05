e88375f06afb950a15ebf4f363e10fa59dbd9e3b
rungene <rungenesamuel@gmail.com>
2023-08-05 12:01:11 +0000

    Task: 3. Printing teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,15 @@ export interface Teacher {
   location: string
   [key: string]: any;
 }
+
+export interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+export interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName: string, LastName: string) {
+  return `${firstName[0]}. ${lastName}`;
+};
