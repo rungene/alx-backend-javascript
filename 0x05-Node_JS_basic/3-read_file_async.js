cfe7e709a864f6b7155342aed1a0010fd6f3ea9e
rungene <rungenesamuel@gmail.com>
2023-09-20 01:49:43 +0000

    Returning promise

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,33 +1,37 @@
 const fs = require('fs');
 
 function countStudents(filePath) {
-  fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-      throw new Error('Cannot load the database');
-    }
-    const sweCount = [];
-    const csCount = [];
-    const lines = data.split('\n').slice(1);
-    for (let i = 0; i < lines.length; i += 1) {
-      const line = lines[i].trim();
-      const words = line.split(',');
-      if (words.length > 0) {
-        const lastWord = words[words.length - 1];
-        if (lastWord === 'CS') {
-          csCount.push(words[0]);
-        } else if (lastWord === 'SWE') {
-          sweCount.push(words[0]);
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const sweCount = [];
+        const csCount = [];
+        const lines = data.split('\n').slice(1);
+        for (let i = 0; i < lines.length; i += 1) {
+          const line = lines[i].trim();
+          const words = line.split(',');
+          if (words.length > 0) {
+            const lastWord = words[words.length - 1];
+            if (lastWord === 'CS') {
+              csCount.push(words[0]);
+            } else if (lastWord === 'SWE') {
+              sweCount.push(words[0]);
+            }
+          }
         }
+        const totalStudents = sweCount.length + csCount.length;
+        console.log(`Number of students: ${totalStudents}`);
+        if (csCount.length > 0) {
+          console.log(`Number of students in CS: ${csCount.length}. List: ${csCount.join(', ')}`);
+        }
+        if (sweCount.length > 0) {
+          console.log(`Number of students in SWE: ${sweCount.length}. List: ${sweCount.join(', ')}`);
+        }
+        resolve();
       }
-    }
-    const totalStudents = sweCount.length + csCount.length;
-    console.log(`Number of students: ${totalStudents}`);
-    if (csCount.length > 0) {
-      console.log(`Number of students in CS: ${csCount.length}. List: ${csCount.join(', ')}`);
-    }
-    if (sweCount.length > 0) {
-      console.log(`Number of students in SWE: ${sweCount.length}. List: ${sweCount.join(', ')}`);
-    }
+    });
   });
 }
 module.exports = countStudents;
