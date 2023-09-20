33682f01e3a80936fb4bf00029936035885ce1e0
rungene <rungenesamuel@gmail.com>
2023-09-20 01:23:10 +0000

    Eslint compliant

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -4,7 +4,6 @@ function countStudents(filePath) {
   fs.readFile(filePath, 'utf-8', (err, data) => {
     if (err) {
       throw new Error('Cannot load the database');
-      return;
     }
     const sweCount = [];
     const csCount = [];
@@ -29,7 +28,6 @@ function countStudents(filePath) {
     if (sweCount.length > 0) {
       console.log(`Number of students in SWE: ${sweCount.length}. List: ${sweCount.join(', ')}`);
     }
-
   });
 }
 module.exports = countStudents;
