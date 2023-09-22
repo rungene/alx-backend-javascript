b0a1d1f77bad2bf8b47b9eb0bdc99452f7458826
rungene <rungenesamuel@gmail.com>
2023-09-22 08:57:20 +0000

    Corrections

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -9,6 +9,7 @@ describe('calculateNumber', function() {
   it('Adds 1 and 3 correctly', testCalculateNumber({a: 1, b: 3, expected: 4}));
   it('Adds 1 and 3.7 correctly', testCalculateNumber({a: 1, b: 3.7, expected: 5}));
   it('Adds 1.2 and 3.7 correctly', testCalculateNumber({a: 1.2, b: 3.7, expected: 5}));
+  it('Adds 1.3 and 3 correctly', testCalculateNumber({a: 1.3, b: 3, expected: 4}));
   it('Adds 1.5 and 3.7 correctly', testCalculateNumber({a: 1.5, b: 3.7, expected: 6}));
   it('Adds -1 and -3 correctly', testCalculateNumber({a: -1, b: -3, expected: -4}));
   it('Adds 2000 and 3000 correctly', testCalculateNumber({a: 2000, b: 3000, expected: 5000}));
